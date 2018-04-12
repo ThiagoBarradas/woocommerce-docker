db49f447a8e6aad554bebec92ef0e06cbb9ba573
Thiago Barradas <thiagobrowskt@gmail.com>
2018-04-11 23:52:04 -0300

    add new versions

--- images/2.6.11-wp4.7.2-php7.1/scripts/setup-woocommerce.sh
+++ images/2.6.11-wp4.7.2-php7.1/scripts/setup-woocommerce.sh
@@ -1,4 +1,7 @@
-sleep 60
+while ! /etc/init.d/mysql status | grep -m1 'is running'; do
+    sleep 1
+    echo "Waiting for mysql service..."
+done
 
 wp plugin install woocommerce --version=$WOOCOMMERCE_VERSION --allow-root --activate
 wp theme install storefront --allow-root --activate 
@@ -6,7 +9,7 @@ wp plugin install woocommerce-extra-checkout-fields-for-brazil --activate --allo
 wp plugin install wordpress-importer --activate --allow-root 
 php -f /setup-wizard-woocommerce.php
 chmod -R 777 /app 
-wp import /app/wp-content/plugins/woocommerce/dummy-data/dummy-data.xml --authors=create --allow-root 
+wp import /app/wp-content/plugins/woocommerce/*/*.xml --authors=create --allow-root 
 
 chmod -R 777 /app 
-rm -f /etc/supervisor/conf.d/supervisord-zwordpress.conf
+rm -f /etc/supervisor/conf.d/supervisord-zwordpress.confs
